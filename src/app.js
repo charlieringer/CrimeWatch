3eeb22f85a0b7ef46bd5e9e79d0c35fbe940efb0
amrishparmar <amrish.parmar@me.com>
2016-04-23 21:09:19 +0100

    Added ajax fetching of police data

--- src/app.js
+++ src/app.js
@@ -11,27 +11,6 @@ var Vibe = require('ui/vibe');
 var updateTime = 0;
 
 var watchId;
-function success(pos) {
-  console.log('Location changed!');
-  console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
-
-}
-
-function error(err) {
-  console.log('location error (' + err.code + '): ' + err.message);
-}
-
-var options = {
-  enableHighAccuracy: true,
-  maximumAge: 0,
-  timeout: 5000
-};
-
-// Get location updates
-watchId = navigator.geolocation.watchPosition(success, error, options);
-
-// Clear the watch and stop receiving updates
-navigator.geolocation.clearWatch(watchId);
 
 var main = new UI.Card({
   title: 'Crime Watch',
@@ -44,7 +23,6 @@ var main = new UI.Card({
 main.show();
 
 main.on('click', 'up', function(e) {
-  numbCrimes++;
   Vibe.vibrate('long');
 });
 
@@ -65,7 +43,7 @@ main.on('click', 'select', function(e) {
   });
   menu.show();
   menu.on('select', function(e) {
-    if(e.itemIndex == 0)
+    if(e.itemIndex === 0)
       {
         drawUpdateFreq();
       } else if (e.itemIndex == 1){
@@ -98,7 +76,7 @@ function drawUpdateFreq(){
   });
   menu.show();
   menu.on('select', function(e) {
-    if(e.itemIndex == 0)
+    if(e.itemIndex === 0)
       {
         updateTime = 10;
       } else if (e.itemIndex == 1){
@@ -139,3 +117,49 @@ function drawNotifications()
   });
   menu.show();
 }
+
+function success(pos) {
+  console.log('Location changed!');
+  var lat = pos.coords.latitude;
+  var long = pos.coords.longitude;
+  console.log('lat= ' + lat + ' lon= ' + long);
+  var url = 'https://data.police.uk/api/crimes-street/all-crime?lat=' + lat + '&lng=' + long + '&date=2016-02';
+  ajax(
+    {
+      url: url,
+      type: 'json'
+    },
+    function(data) {
+      // Success!
+      console.log("Successfully fetched crime data!");
+      var crimeCount = 0;
+      for (var i = 0; i < data.length; ++i) {
+        var obj = data[i];
+        if (obj.category === "Violence and sexual offences" || obj.category === "Theft from the person") {
+          ++crimeCount;
+        }
+      }
+      main.body('Number of crimes in last ' + 1 + ' month(s): ' + crimeCount);
+    },
+    function(error) {
+      // Failure!
+      console.log('Failed fetching crime data: ' + error);
+    }
+  );
+}
+
+function error(err) {
+  console.log('location error (' + err.code + '): ' + err.message);
+}
+
+var options = {
+  enableHighAccuracy: true,
+  maximumAge: 0,
+  timeout: 5000
+};
+
+// Get location updates
+watchId = navigator.geolocation.watchPosition(success, error, options);
+
+// Clear the watch and stop receiving updates
+navigator.geolocation.clearWatch(watchId);
